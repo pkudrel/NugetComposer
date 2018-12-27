949d3099fe678a9f97a9d2f8563c711f0783dad3
Piotr Kudrel <piotr.kudrel@deneblab.com>
2018-12-27 15:15:55 +0100

    Add source files

--- src/NugetComposer/Program.cs
+++ src/NugetComposer/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using NugetComposer.Common.Bootstrap;
+using NugetComposer.Feature.Arguments;
 
 namespace NugetComposer
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            try
+            {
+                MainAsync(args).GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
+
+
+        private static async Task<int> MainAsync(string[] args)
+        {
+            //var args = new[] { "-n"};
+
+            var env = Boot.Instance.GetAppEnvironment();
+            var app = CommandLineDefinition.Make(env);
+            return app.Execute(args);
+                
+        }
+    }
+
+    internal class Make
+    {
     }
 }
